6a3ca2cd450d10851a9784e4f31137ad974a6280
harakte <csm.harakte@gmail.com>
2022-09-21 02:15:22 +0900

    fix(JPA): batch insert 수정
    batch insert 문제 수정

--- src/main/java/com/harakte/searchblog/service/SearchBlogService.java
+++ src/main/java/com/harakte/searchblog/service/SearchBlogService.java
@@ -67,39 +67,6 @@ public class SearchBlogService {
         return resDto;
     }
 
-    private List<BlogDto> getBlogs(String word){
-        Keyword keyword = getKeyword(word);
-        if(keyword.getBlogs() == null || keyword.getBlogs().isEmpty()){
-            return new ArrayList<>();
-        }
-
-        return keyword.getBlogs().stream()
-                .map(BlogInfoMapper.INSTANCE::getBlogDto)
-                .collect(Collectors.toList());
-    }
-
-    public Keyword getKeyword(String word){
-        Keyword keyword = keywordRepository.findFirstByWord(word);
-        if(keyword != null){
-            keyword.setSearchCount(keyword.getSearchCount() + 1);
-            keyword.setUpdDateTime(OffsetDateTime.now(ZoneOffset.UTC));
-            return keywordRepository.save(keyword);
-        }
-
-        Keyword keywordToInsert = new Keyword();
-        keywordToInsert.setWord(word);
-        keywordToInsert.setSearchCount(1);
-        keywordToInsert.setCreateDate(OffsetDateTime.now(ZoneOffset.UTC));
-
-        List<BlogDto> blogDtos = kakaoManager.getBlogs(word);
-        List<Blog> blogs = blogDtos.stream()
-                .map(blogInfoDto -> BlogInfoMapper.INSTANCE.getBlog(keywordToInsert, blogInfoDto))
-                .collect(Collectors.toList());
-        keywordToInsert.setBlogs(blogs);
-
-        return keywordRepository.save(keywordToInsert);
-    }
-
     public GetPopularKeywordsResDto getPopularKeywords(GetPopularKeywordsReqDto reqDto){
         int size = 10;
         if(reqDto.getSize() == null){
@@ -111,5 +78,42 @@ public class SearchBlogService {
                 .map(KeywordMapper.INSTANCE::getKeywordDto)
                 .collect(Collectors.toList());
         return new GetPopularKeywordsResDto(keywordDtos);
+    }
+
+    private List<BlogDto> getBlogs(String word){
+        Keyword keyword = getKeyword(word);
+        if(keyword.getBlogs() == null || keyword.getBlogs().isEmpty()){
+            return new ArrayList<>();
+        }
+
+        return keyword.getBlogs().stream()
+                .map(BlogInfoMapper.INSTANCE::getBlogDto)
+                .collect(Collectors.toList());
+    }
+
+    private Keyword getKeyword(String word){
+        Keyword keyword = keywordRepository.findFirstByWord(word);
+        if(keyword != null){
+            keyword.setSearchCount(keyword.getSearchCount() + 1);
+            keyword.setUpdDateTime(OffsetDateTime.now(ZoneOffset.UTC));
+            return keywordRepository.save(keyword);
+        }
+
+        return insertKeyword(word);
+    }
+
+    private Keyword insertKeyword(String word){
+        Keyword keyword = new Keyword();
+        keyword.setWord(word);
+        keyword.setSearchCount(1);
+        keyword.setCreateDate(OffsetDateTime.now(ZoneOffset.UTC));
+
+        List<BlogDto> blogDtos = kakaoManager.getBlogs(word);
+        List<Blog> blogs = blogDtos.stream()
+                .map(blogInfoDto -> BlogInfoMapper.INSTANCE.getBlog(keyword, blogInfoDto))
+                .collect(Collectors.toList());
+        keyword.setBlogs(blogs);
+
+        return keywordRepository.save(keyword);
     }
 }
